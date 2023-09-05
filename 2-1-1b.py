3315bb374b0a8790ecf5e6343f7a9b8d6f1055be
Taehyung Park <34673295+tparkSSU@users.noreply.github.com>
2023-09-05 11:48:58 +0900

    Add files via upload

---
+++ 2-1-1b.py
@@ -0,0 +1,25 @@
+print(3 + 2, 3 - 2, 3 * 2)
+print(8 / 2, 8 ** 2, 2 * (3 + 4))
+s = "hello"
+print(s+" "+str(4))
+s1 = "Python"[-2:-5:-1]
+print(s1)
+x = eval("23+3*5")
+print(x)
+x = int(-4.8)
+ss = "a\tb\tc"
+print(ss)
+print(len(ss))
+ss2 = ss
+print(len(ss2))
+print(x)
+thing = 'wereduck'
+place = 'werepond'
+z=f'The {thing} is in the {place}'
+x = 2345.3456
+y = 12.23456
+print("the values is {0:10,.2f} and {1:10.2%}".format(x,y))
+print(f'the value is {x:<10,.2f} and {y:>20.3%}')
+
+print( f'The {thing:>20} is in the {place:.^20} and {x:<10.3f}')
+
